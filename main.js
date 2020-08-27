4a9f7bfb7bab7332d38bf567b3141d3b506247f1
Ivan Herman <ivan@ivan-herman.net>
2020-08-27 17:39:11 +0200

    Basic extraction mechanism

--- main.js
+++ main.js
@@ -2,16 +2,25 @@
 
 'use strict';
 
-const { get_minutes }     = require('./lib/io');
+const fs  = require('fs');
+const fsp = fs.promises;
 const { get_resolutions } = require('./lib/get_resolution');
 
 
-async function main(fname) {
-    const minutes = await get_minutes(fname);
-    const res = get_resolutions(minutes)
+async function main(file_names) {
+    const minutes_promises = file_names.map((file_name) => fsp.readFile(file_name, 'utf-8'));
+    const minutes          = await Promise.all(minutes_promises);
+    const resolutions      = minutes.map(get_resolutions).reduce((accumulator, currentValue) => {
+        if (Object.keys(accumulator).includes(currentValue.year)) {
+            accumulator[currentValue.year] = [...accumulator[currentValue.year], ...currentValue.resolutions];
+        } else {
+            accumulator[currentValue.year] = currentValue.resolutions;
+        }
+        return accumulator;
+    }, {});
 
-    console.log(JSON.stringify(res, null, 4));
+    console.log(JSON.stringify(resolutions, null, 4));
 }
 
 // Do it
-main('tests/2019-05-31-json-ld.md');
+main(['tests/2019-05-31-json-ld.md','tests/2019-05-07-pwg.md','tests/2020-04-21-did.md']);

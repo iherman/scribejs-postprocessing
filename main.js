014e7e44092304100ee4bd2d264d56665179e7dd
Ivan Herman <ivan@ivan-herman.net>
2020-08-31 16:48:20 +0200

    Changes:
    - prepared a 'view' script and corresponding tests
    - handling an exception when a read of the asset cannot be done
    - added an information on the 'call' into the resolution structure, in case it is necessary to filter the minutes into categories (e.g., the did wg case)

--- main.js
+++ main.js
@@ -10,8 +10,20 @@ async function collect_resolutions(file_names) {
     const minutes_promises = file_names.map((file_name) => fsp.readFile(file_name, 'utf-8'));
     const minutes          = await Promise.all(minutes_promises);
     const resolutions      = minutes
+        // extract all resolutions from the markdown text, returning an array of resolution objects
         .map(get_resolutions)
+        // add the group name to each resolution, based on the file name
+        .map((res_array, index) => {
+            // extract the group name from the file short name
+            const call = short_names[index].split('.')[0].slice(11)
+            return res_array.map((resolution) => {
+                resolution.call = call;
+                return resolution;
+            });
+        })
+        // turn array of arrays into a single array
         .reduce((accumulator, currentValue) => [...accumulator, ...currentValue],[])
+        // sort the resolution.
         .sort(sort_resolutions);
 
     return {
@@ -21,15 +33,24 @@ async function collect_resolutions(file_names) {
 }
 
 async function main(file_names) {
-    const current_data    = await fsp.readFile('assets/resolutions.json','utf-8');
-    const current         = JSON.parse(current_data);
+    const FNAME = 'assets/resolutions.json';
+    let current;
+    try {
+        const current_data    = await fsp.readFile(FNAME,'utf-8');
+        current               = JSON.parse(current_data);    
+    } catch(e) {
+        current = {
+            short_names : [],
+            resolutions : []
+        }
+    }
     const missing_files   = filter_resolutions(file_names, current);
     const new_resolutions = await collect_resolutions(missing_files);
     const new_asset       = {
         short_names : [...new_resolutions.short_names, ...current.short_names],
         resolutions : [...new_resolutions.resolutions, ...current.resolutions]
     } 
-    console.log(JSON.stringify(new_asset,null,4));
+    await fsp.writeFile(FNAME, JSON.stringify(new_asset, null, 4), 'utf-8')
 }
 
 

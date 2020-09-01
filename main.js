37228026d423b243b84080eb55263daa5f0ce35e
Ivan Herman <ivan@ivan-herman.net>
2020-09-01 16:08:07 +0200

    What is left to do: upload new set to github

--- main.js
+++ main.js
@@ -1,58 +1,96 @@
 #!/usr/bin/env node
 'use strict';
 
-const fs  = require('fs');
-const fsp = fs.promises;
-const { get_resolutions, sort_resolutions, filter_resolutions } = require('./lib/resolutions');
-
-async function collect_resolutions(file_names) {
-    const short_names      = file_names.map((file_name) => file_name.split('/')).map((splitted) => splitted.pop())
-    const minutes_promises = file_names.map((file_name) => fsp.readFile(file_name, 'utf-8'));
-    const minutes          = await Promise.all(minutes_promises);
-    const resolutions      = minutes
-        // extract all resolutions from the markdown text, returning an array of resolution objects
-        .map(get_resolutions)
-        // add the group name to each resolution, based on the file name
-        .map((res_array, index) => {
-            // extract the group name from the file short name
-            const call = short_names[index].split('.')[0].slice(11)
-            return res_array.map((resolution) => {
-                resolution.call = call;
-                return resolution;
-            });
-        })
-        // turn array of arrays into a single array
-        .reduce((accumulator, currentValue) => [...accumulator, ...currentValue],[])
-        // sort the resolution.
-        .sort(sort_resolutions);
-
-    return {
-        short_names,
-        resolutions,
+const MINUTE_REPOS = [
+    {
+        owner   : 'iherman',
+        repo    : 'scribejs-resolutions',
+        minutes : '_minutes',
+        current : 'assets/resolutions.json'
     }
-}
+]
 
-async function main(file_names) {
-    const FNAME = 'assets/resolutions.json';
-    let current;
+const DEBUG            = true;
+const USER_CONFIG_NAME = ".ghid.json";
+const ASSET_NAME       = 'assets/resolutions.json';
+
+
+/* ------------------------------------------------------------------------------------------------- */
+
+const fs      = require('fs');
+const fsp     = fs.promises;
+const path    = require('path');
+const Octokat = require('octokat');
+const { filter_resolutions, collect_resolutions } = require('./lib/resolutions');
+
+
+/**
+ * The main processing: for each repo in [[minute_repos]] get the current list of resolution from the repo,
+ * compare them to the list of minutes in the `_minutes` folder on the top of the repo, generate the missing 
+ * set of resolutions, and return the updated list.
+ */
+async function main() {
+    /* Used for debugging... */
+    const print = (preamble, obj) => console.log(`${preamble}: ${JSON.stringify(obj, null, 4)}`);
+    const base64_to_string = (data) => Buffer.from(data,'base64').toString('utf-8');
+
+    //1. Get hold of the github credential
+    let github_credentials = {};
     try {
-        const current_data    = await fsp.readFile(FNAME,'utf-8');
-        current               = JSON.parse(current_data);    
+        const fname          = path.join(process.env.HOME, USER_CONFIG_NAME);
+        const config_content = await fsp.readFile(fname, 'utf-8');
+        github_credentials   = JSON.parse(config_content);
     } catch(e) {
-        current = {
-            short_names : [],
-            resolutions : []
-        }
+        console.log(`Could not get hold of the github credentials: ${e}`);
+        process.exit(-1);
     }
-    const missing_files   = filter_resolutions(file_names, current);
-    const new_resolutions = await collect_resolutions(missing_files);
-    const new_asset       = {
-        short_names : [...new_resolutions.short_names, ...current.short_names],
-        resolutions : [...new_resolutions.resolutions, ...current.resolutions]
-    } 
-    await fsp.writeFile(FNAME, JSON.stringify(new_asset, null, 4), 'utf-8')
+    // if (DEBUG) print('Credentials:', github_credentials);
+
+    // Do what is necessary for each github repositories
+    MINUTE_REPOS.forEach(async (repo) => {
+        try {
+            // if (DEBUG) print('Repo: ', repo);
+            const the_repo = (new Octokat({ token: github_credentials.ghtoken })).repos(repo.owner, repo.repo);
+
+            // Get hold of the asset file to see what is there...
+            let current;
+            try {
+                const current_assets_wrapper = await the_repo.contents(repo.current).fetch();
+                current                      = JSON.parse(base64_to_string(current_assets_wrapper.content));    
+            } catch(e) {
+                current = {
+                    short_names : [],
+                    resolutions : []
+                }        
+            }
+            // if (DEBUG) print('Current assets', current);  
+
+            // Get hold of the list of minute files
+            const list_of_minutes_wrappers = await the_repo.contents(repo.minutes).fetch({ per_page: 400 });
+            const list_of_minutes          = list_of_minutes_wrappers.items.map((item) => item.downloadUrl.split('/').pop());
+            // if (DEBUG) print('Current listing', list_of_minutes);
+
+            const missing_files = filter_resolutions(list_of_minutes, current);
+            // if (DEBUG) print('To be used for new resolutions', missing_files);
+
+            const new_resolutions = await collect_resolutions(missing_files, (file_name) => 
+                the_repo.contents(repo.minutes, file_name).read()
+            );
+            // if (DEBUG) print('New set of resolutions', new_resolutions);
+
+            const new_asset       = {
+                short_names : [...current.short_names, ...new_resolutions.short_names],
+                resolutions : [...new_resolutions.resolutions, ...current.resolutions]
+            } 
+            if (DEBUG) print('New asset', new_asset);
+            
+        } catch(e) {
+            console.log(`Problems with repo ${repo}: ${e}`)
+        }
+    });
 }
 
 
-// Do it
-main(process.argv.slice(2))
+
+// Let's go
+main();

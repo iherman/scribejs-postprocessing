c5df4a9794f5c1c9c993e1eb9c0288d43cfb8591
Ivan Herman <ivan@ivan-herman.net>
2020-09-03 16:42:05 +0200

    Essentially done...

--- main.js
+++ main.js
@@ -4,6 +4,13 @@
 const { USER_CONFIG_NAME, LOCAL_REPOS, GITHUB_REPOS, LOG, DEBUG } = require('./lib/config');
 const { github_repos, local_repos } = require('./lib/repos');
 
+/**
+ * Entry point: a simple switch between handling local repos (see {@link local_repos}) and github repos (see {@link github_repos}).
+ * 
+ * The switch between local and github is based on the presence or not of the `-l` (or `--local`) flag.
+ * 
+ * @async
+ */
 async function main() {
     const flag = process.argv.pop();
     flag === '-l' || flag === '--local' ? local_repos() : github_repos();
